d366604c12fd54c745d47012b951df49deb49ad1
Artyom Krasnyuk <akrasnyuk@eshopworld.com>
2019-10-10 13:12:41 +0300

    Added: launch success/failure

--- src/app/pages/pages/launches/launches.module.ts
+++ src/app/pages/pages/launches/launches.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LaunchesRoutingModule } from './launches-routing.module';
 import { LaunchesPageComponent } from './pages/launches-page/launches-page.component';
-import {MatCardModule} from "@angular/material";
+import {MatButtonModule, MatCardModule, MatIconModule, MatTooltipModule} from "@angular/material";
 import { LaunchCardComponent } from './components/launch-card/launch-card.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
 
@@ -14,7 +14,10 @@ import {FlexLayoutModule} from "@angular/flex-layout";
     CommonModule,
     LaunchesRoutingModule,
     MatCardModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ]
 })
 export class LaunchesModule { }
