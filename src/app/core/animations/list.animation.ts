f196a2c32cf4810ba0aa6f29eb0b916b303e462b
Artyom Krasnyuk <akrasnyuk@eshopworld.com>
2019-10-25 15:54:52 +0300

    Fixed: animations

--- src/app/core/animations/list.animation.ts
+++ src/app/core/animations/list.animation.ts
@@ -5,18 +5,24 @@ export const listAnimation = [
   trigger('listAnimation', [
     transition(':enter', [
       // child animation selector + stagger
-      query('@listItemAnimation',
+      query('@listItemEnterAnimation',
         stagger(150, animateChild())
+      ),
+      query('@listItemLeaveAnimation',
+        stagger(100, animateChild()),
+        {optional: true}
       )
     ]),
   ]),
-  trigger('listItemAnimation', [
+  trigger('listItemEnterAnimation', [
     // cubic-bezier for a tiny bouncing feel
     transition(':enter', [
       style({ transform: 'scale(0.5)', opacity: 0 }),
       animate('1s cubic-bezier(.8,-0.6,0.2,1.5)',
         style({ transform: 'scale(1)', opacity: 1 }))
     ]),
+  ]),
+  trigger('listItemLeaveAnimation', [
     transition(':leave', [
       style({ transform: 'scale(1)', opacity: 1, height: '*' }),
       animate('1s cubic-bezier(.8,-0.6,0.2,1.5)',
